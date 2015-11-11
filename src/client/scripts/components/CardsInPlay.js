73d41cf1d0945255df044365ccc0d31039eae2f5
Vincent Lemeunier <vincentlemeunier+git@gmail.com>
2015-11-11 15:16:20 +0100

    :lipstick: Centering card piles in cards-in-play

--- src/client/scripts/components/CardsInPlay.js
+++ src/client/scripts/components/CardsInPlay.js
@@ -3,14 +3,14 @@ import CardPile from './CardPile';
 export default ({piles}) => (
     <div className="cards-in-play">
         <div>Cartes en jeux</div>
-        <div className="cards-in-play__piles">
+        <ul className="cards-in-play__piles">
             {
                 piles.map((pile, index) => (
-                    <div className="cards-in-play__pile" key={index}>
+                    <li className="cards-in-play__pile" key={index}>
                         <CardPile className="card-pile--in-play" cards={pile} />
-                    </div>
+                    </li>
                 ))
             }
-        </div>
+        </ul>
     </div>
 );
