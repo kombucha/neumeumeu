b8d10f303dd1503434b14fa917423be15334b580
Vincent Lemeunier <vincentlemeunier+git@gmail.com>
2015-11-17 22:19:34 +0100

    Username availability

--- src/server/services/player.js
+++ src/server/services/player.js
@@ -3,3 +3,18 @@ import r from 'server/database';
 export function getPlayer(playerId) {
     return r.table('player').get(playerId).run();
 }
+
+export function register() {
+    // TODO
+}
+
+export function isNameAvailable(name) {
+    return r.table('player')
+        .filter({name}).count()
+        .run()
+        .then(count => count === 0);
+}
+
+export function logout() {
+    // TODO
+}
