df19496159c339360b1613b027eb170aac2c63d0
Vincent Lemeunier <vincentlemeunier+git@gmail.com>
2015-11-18 22:36:12 +0100

    Started implementing auth stuff on the server

--- src/server/services/player.js
+++ src/server/services/player.js
@@ -4,17 +4,9 @@ export function getPlayer(playerId) {
     return r.table('player').get(playerId).run();
 }
 
-export function register() {
-    // TODO
-}
-
 export function isNameAvailable(name) {
     return r.table('player')
         .filter({name}).count()
         .run()
         .then(count => count === 0);
-}
-
-export function logout() {
-    // TODO
 }
